779bc193e6472d7c2fa2d3e7d27e9f8d29515cdc
alanxudai@gmail.com <alanxudai@gmail.com>
2020-11-25 19:19:26 -0800

    implement an all todos selector util

--- W10/D3/todos/frontend/components/root.jsx
+++ W10/D3/todos/frontend/components/root.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App from './app';
 
-const Root = (props) => {
+const Root = ({ store }) => {
     return (
-        <Provider store={props.store}>
+        <Provider store={store}>
             <App />
         </Provider>
     )
