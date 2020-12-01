60c48508043c2011ac7c4a2853c9b76e61bb6654
alanxudai@gmail.com <alanxudai@gmail.com>
2020-11-30 17:09:21 -0800

    implement rails backend and connected it to a react-redux front-end

---
+++ W11/D1/todos/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create({title: "buy cat", body: "bigger cats are better", done: false})
+Todo.create({title: "seed that thunky thunk", body: "contact thunk daddy", done: true})
+Todo.create({title: "wait for mike to pop in", body: "he needs to help me", done: false})
+Todo.create({title: "learn to interact with real humans again", body: "pandemic social awkwardness is real", done: false})
+Todo.create({title: "fill out seed db", body: "boring stuff", done: true})
+Todo.create({title: "sell Alan my cat", body: "I am not a fan of cats", done: false})
+Todo.create({title: "take a nap", body: "I am tired", done: false})
+Todo.create({title: "hit the lottery", body: "working sucks", done: false})
+Todo.create({title: "Create last seed", body: "it's hard to think of seeds", done: true})
---
+++ W11/D1/todos/frontend/util/todo_api_util.js
@@ -0,0 +1,7 @@
+
+export const fetchTodos = () => (
+    $.ajax({
+        url: "api/todos",
+        method: "GET",
+    })
+)
