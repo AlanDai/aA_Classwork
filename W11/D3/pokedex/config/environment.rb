167e0f510dbf3b570c2be8534a30798610e046dc
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-02 11:59:52 -0800

    continue from W11/D2/pokedex

---
+++ W11/D3/pokedex/config/environment.rb
@@ -0,0 +1,8 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+# format Jbuilder keys.
+Jbuilder.key_format camelize: :lower
